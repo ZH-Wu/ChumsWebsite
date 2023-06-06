a5fdcdef4d404b92e28ca87c2e5433b3f2b3754d
ZH_Wu <zhtomwu@gmail.com>
2023-06-06 23:18:51 +0800

    新增awd檔案

--- js/product.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  $('.dropdown').click(function() {
-    $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
-    $(this).siblings().slideToggle();
-  });
-});
