d607113e2441f8094fa6aaae5bc4708150089d93
ZH_Wu <zhtomwu@gmail.com>
2023-06-19 17:17:16 +0800

    更新header.html樣 式

--- js/header.js
+++ js/header.js
@@ -1,7 +1,13 @@
 $(function () {
+  // header下拉式選單
   $('.dropdown').click(function () {
     $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
     $(this).siblings().slideToggle();
+  });
+
+  // offcanvas_RWD滑動式選單
+  $('.menuL, .back').click(function() {
+    $('.menuL, .back, .slide-menuM').toggleClass('active');
   });
 
   // 簡化前的程式碼
--- js/story.js
+++ js/story.js
@@ -4,6 +4,7 @@ $(function () {
     $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
     $(this).siblings().slideToggle();
   });
+
   // offcanvas_RWD滑動式選單
   $('.menuL, .back').click(function() {
     $('.menuL, .back, .slide-menuM').toggleClass('active');
