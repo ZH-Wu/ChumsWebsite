608d5b8c8fc10ae729ac12a57748ae6f1c5f4d76
ZH_Wu <zhtomwu@gmail.com>
2023-06-20 18:56:12 +0800

    更新圖片路徑

--- js/store.js
+++ js/store.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  // header下拉式選單
+  $('.dropdown').click(function () {
+    $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
+    $(this).siblings().slideToggle();
+  });
+
+  // offcanvas_RWD滑動式選單
+  $('.menuL, .back').click(function () {
+    $('.menuL, .back, .slide-menuM').toggleClass('active');
+  });
+
   $("#n,#n .img_c").click(function () {
     $(".list_item_m,.list_item_s").hide("slow");
   });
@@ -17,23 +28,4 @@ $(document).ready(function () {
   $("#s").click(function () {
     $(".list_item_s").show("slow");
   });
-});
-$(function () {
-  // header下拉式選單
-  $('.dropdown').click(function () {
-    $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
-    $(this).siblings().slideToggle();
-  });
-
-  // offcanvas_RWD滑動式選單
-  $('.menuL, .back').click(function () {
-    $('.menuL, .back, .slide-menuM').toggleClass('active');
-  });
-
-  // 簡化前的程式碼
-  // $('.dropdown').click(function() {
-  //   $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active');
-  //   $(this).siblings().slideToggle();
-  //   $(this).parent().siblings().find('.dropdown-item').slideUp();
-  // });
 });
