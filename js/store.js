2d08dbc8a3e74dc4916680cf7738c22c639da0f5
ZH_Wu <zhtomwu@gmail.com>
2023-06-25 12:27:27 +0800

    復原

--- js/store.js
+++ js/store.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
   // header下拉式選單
-  $('.dropdown').click(function () {
+  $('.dropdown').click(function (e) {
+    e.stopPropagation();
     $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active').siblings().slideUp();
     $(this).siblings().slideToggle();
   });
-
+  $('header, main').click(function () {
+    $('.dropdown-item').slideUp();
+  });
   // offcanvas_RWD滑動式選單
   $('.menuL, .back').click(function () {
     $('.menuL, .back, .slide-menuM').toggleClass('active');
